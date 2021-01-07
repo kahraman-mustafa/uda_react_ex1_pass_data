b7ab48aba351f3e6dd6e8691b414ee7c0902373a
Mustafa Kahraman <kahramanmstafa@gmail.com>
2021-01-07 21:36:42 +0300

    Exercise 1 için beklenen sonuç yazdırıldı.

---
+++ pass_data/src/Movie.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react"
+
+
+class Movie extends Component {
+	render(){
+        const user = this.props.user
+        const profiles = this.props.profiles
+        const movies = this.props.movies
+
+        const movieID = profiles.filter(
+            (profile) => profile.userID === user.id.toString()
+          ).map((profile) => (profile.favoriteMovieID))
+
+        const movieName = Object.keys(movies)
+          .filter((key) => movies[key].id.toString() === movieID[0])
+          .map((key) => movies[key].name);
+
+    	return <p>{user.name}'s favorite movie is {movieName[0]}</p>;
+    }  
+}
+
+export default Movie;
